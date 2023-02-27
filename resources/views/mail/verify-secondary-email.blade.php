c024a5a3a234da08e6daab7372177180b37c5761
nnataali <natalia.charkviani.1@btu.edu.ge>
2023-02-27 17:14:32 +0400

    fix: email styles

--- resources/views/mail/verify-secondary-email.blade.php
+++ resources/views/mail/verify-secondary-email.blade.php
@@ -7,18 +7,17 @@
        }
    </style>
 
-
    <body style="background: #181624; color:white;font-family: 'Montserrat', sans-serif; word-wrap: break-word;">
-       <div style="display: flex; flex-direction:column; gap: 2px;   justify-content: center; padding:3% 10% ">
-           <img src="{{ asset('images/notification.png') }}" alt="notification" style="width: 22px; margin: 0 auto">
-           <div style=" margin-bottom: 65px text-align: center; font-size: 12px; margin:auto">
-               <h1 style="color:#DDCCAA; font-weight: 500">{{ __('mail.movie_qutes') }}</h1>
-           </div>
-           <p style="top: 25px">{{ __('mail.hola') }} {{ $user->name }} ! </p>
-           <p style="margin-bottom: 32px">{{ __('mail.verify_welcome_text') }}</p>
+       <div style="  justify-content: center; padding:3% 10% ">
+            <div style="width:100%; text-align:center; padding-bottom:50px">
+                <img src="{{ asset('images/notification.png') }}" alt="notification">
+                <h1 style="font-size:18px;color:#DDCCAA; padding-top:9px">{{ __('mail.movie_qutes') }}</h1>
+            </div>
+           <p style="top: 30px">{{ __('mail.hola') }} {{ $user->name }} ! </p>
+           <p style="margin-bottom: 38px">{{ __('mail.verify_welcome_text') }}</p>
            <a href="{{  config('app.frontend_url'). '/'. $local.'/profile?token='.$email->token.'&email='.$email->email }}"
                style=" padding:7px 10px; text-align:center; max-width: 200px; color:white;background:#E31221;text-decoration: none;font-weight: 400; border-radius:5px">{{ __('mail.verify_account') }}</a>
-           <p >{{ __('mail.if_doesnot_work') }}</p>
+           <p style="margin-bottom: 20px" >{{ __('mail.if_doesnot_work') }}</p>
            <a href="{{ config('app.frontend_url').  '/'.$local.'/profile?token='.$email->token.'&email='.$email->email }}"
                style="margin-bottom: 20px;color:#DDCCAA;text-decoration: none;cursor: pointer">
             {{  config('app.frontend_url'). '/'. $local.'/profile?token='.$email->token.'&email='.$email->email }}   </a>
